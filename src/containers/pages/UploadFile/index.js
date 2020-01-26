b1ac791682dd57fb60d04082e4cd8d9bf211ea79
aswar <aswarmp60@gmail.com>
2020-01-26 20:58:44 +0700

    Tambah Halaman Upload

--- src/containers/pages/UploadFile/index.js
+++ src/containers/pages/UploadFile/index.js
@@ -7,37 +7,47 @@ import {
     Image
 } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+// import ImagePicker from "react-native-image-picker";
+
+// const options = {
+//         title: 'Pilih Foto',
+//         takePhotoButtonTitle:'ambil Gambar',
+//         chooseFromLibraryButtonTitle: 'Pilih Fofo dipenyimpanan',
+//         quality:1
+// };
 
 class UploadFile extends Component {
     render() {
         return (
-                <View style={{flex:1,backgroundColor:'#9B9B9B',}}>
-                    <View style={{backgroundColor:'#FFFFFF',borderRadius:7, height:180, 
-                        marginHorizontal:12, marginTop:12 }}>
-                        <Text style={{fontSize:15,
-                        marginTop:16, marginHorizontal:12,
-                        borderStyle:'solid' }}>Upload Surat Jalan</Text>
-                        <View style={{borderStyle:'solid', borderBottomWidth:1, borderColor:'#B8B8B8', marginHorizontal:12,}}></View>
-                        
-                        <View style={{marginLeft:12}}>
-                            <Text style={{fontSize:10, color:'#B8B8B8',}}
-                            >Upload Surat jalan Untuk di Verifikasi Oleh admin</Text>
-                        </View>
-
-                        <TouchableOpacity>
-                            <View style={{alignItems:'center', justifyContent:'center', marginTop:30, }}>
-                                <Image source={require('../../../../assets/logo/upload.png')} style={{width:50,height:50}}/>
-                                <Text style={{marginTop:10,fontSize:10, }}>Upload File</Text>
-                            </View>
-                        </TouchableOpacity>
-                        
+                // Untuk Background
+                <View style={{flex:1,backgroundColor:'#9B9B9B'}}>
+                    {/* Untuk Border*/}
+                    <TouchableOpacity>
+                    <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center',marginTop:60,
+                    borderWidth:1, marginHorizontal:50, borderRadius:5, height:60, borderColor:'#ffffff'}}>
+                        <Image source={require('../../../../assets/icon/upload.png')} style={{width:20,height:20}}/>
+                        <Text style={{marginLeft:12, fontSize:10, fontWeight:'bold'}}>Upload Surat Jalan</Text>
                     </View>
-                    {/* <View style={{marginTop:315,}} >
-                        <Button
-                            title = ""
-                            color = "red" style={{}}
-                        />
-                    </View> */}
+                    </TouchableOpacity>
+                    {/* Untuk gambar */}
+                    <View style={{marginTop:30, marginHorizontal:50,}}>
+                        <View style={{flexDirection:'row', }}>
+                            <View style={{backgroundColor:'#fd5e53', borderRadius:5, height:150, alignContent:'center',
+                            alignItems:'center', flex:1}}>
+                                <Image source={require('../../../../assets/logo/file2.jpg')} style={{width:70,height:90, marginTop:19}}/>
+                                <Text style={{fontSize:10, fontWeight:'bold',
+                                marginTop:10, color:'#ffffff'
+                                }}> Surat Jalan</Text>
+                            </View>
+                            <View style={{ flex:1, marginLeft:19,backgroundColor:'#fd5e53', borderRadius:5, height:150, alignContent:'center',
+                            alignItems:'center'}}>
+                                <Image source={require('../../../../assets/logo/file2.jpg')} style={{width:70,height:90, marginTop:19}}/>
+                                <Text style={{fontSize:10, fontWeight:'bold',
+                                marginTop:12, color:'#ffffff'
+                                }}> Surat Jalan</Text>
+                            </View>
+                        </View>
+                    </View>
                 </View>
         );
     }
